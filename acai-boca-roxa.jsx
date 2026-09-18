3e5767952004131922b910db6357be5d5c435e5a
Kauan Rodrigues Santos <rodrigueskauan07@gmail.com>
2026-09-18 15:57:20 -0300

    fix: atualizar taxa de delivery do Cond. La Font para R$ 9

--- acai-boca-roxa.jsx
+++ acai-boca-roxa.jsx
@@ -223,7 +223,7 @@ async function disablePushNotifications() {
     "Ermida Dom Bosco": 15,
     "Del Lago 11": 6,
     "Baixinho": 7,
-    "Cond. La Font": 8,
+    "Cond. La Font": 9,
     "Cond. Entre Lagos": 8,
     "Cond. Novo Horizonte": 7,
     "Cond. Porto Seguro": 10,
